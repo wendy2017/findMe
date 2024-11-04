5b28b273276ff49283bfe08f328acd93e008bbd1
wendy <lilinvv@163.com>
2024-11-04 13:59:40 +0800

    chore(*): add Referrer-Policy: no-referer

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/projects/biShow",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
