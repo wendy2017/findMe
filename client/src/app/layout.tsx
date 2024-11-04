27d326220ea184fe8194c4bebf7017b08d0a80f1
wendy <lilinvv@163.com>
2024-11-04 11:55:26 +0800

    chore(*): header refer

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import DashboardWrapper from "./dashboardWrapper";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="referrer" content="no-referrer" />
-        <title>FindMe</title>
-      </Head>
       <body className={inter.className}>
         <DashboardWrapper>{children}</DashboardWrapper>
       </body>
