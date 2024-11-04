61fcb6fc756e243aff88341f5adbfd47714e258c
wendy <lilinvv@163.com>
2024-11-04 12:27:24 +0800

    chore(*): fix 403

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import DashboardWrapper from "./dashboardWrapper";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="referrer" content="no-referrer" />
+      </Head>
       <body className={inter.className}>
         <DashboardWrapper>{children}</DashboardWrapper>
       </body>
